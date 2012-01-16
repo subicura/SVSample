46573c829508d939e68b5c23bcdf436560661437
subicura <subicura@subicura.com>
2012-01-16 09:05:45 +0900

    Initial Commit

---
+++ SVSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SVSample
+//
+//  Created by subicura on 12. 1. 16..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SVSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SVSample
+//
+//  Created by subicura on 12. 1. 16..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
